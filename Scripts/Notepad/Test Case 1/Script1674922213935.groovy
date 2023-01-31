38cc3338fa4865e0a36ee489ceed0374aa353161
ttvquang <ttvquang@gmail.com>
2023-02-01 00:55:38 +0700

    Create Calculator test case

---
+++ Scripts/Notepad/Test Case 1/Script1674922213935.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Windows.startApplication('C:/windows/System32/notepad.exe')
+
+Windows.delay(1)
+
+Windows.setText(findWindowsObject('Object Repository/Notepad/Text Editor Area'), '===== Happy Hours =====')
+
+Windows.click(findWindowsObject('Notepad/Menu-File item'))
+
+Windows.switchToWindow(findWindowsObject('Object Repository/Notepad/Root Windows Object'))
+
+Windows.click(findWindowsObject('Object Repository/Notepad/Save as item'))
+
+Windows.setText(findWindowsObject('Notepad/File name combobox by xpath'), 'notepad_testing.txt')
+
+Windows.click(findWindowsObject('Notepad/Save Button in Save As window'))
+
+Windows.closeApplication()
+
